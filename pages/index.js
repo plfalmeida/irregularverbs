5a14a0cc69c54404fe8c751a86837011b374a0b7
Pedro Almeida <pedro--luiz@outlook.com>
2020-11-01 17:40:22 -0300

    chore: test url

--- pages/index.js
+++ pages/index.js
@@ -67,7 +67,7 @@ export default function Home({ verbs = [] }) {
 }
 
 export const getStaticProps = async () => {
-  const response = await fetch('https://irregularverbs.vercel.app/api/verbs');
+  const response = await fetch('/api/verbs');
   const verbs = await response.json();
   return {
     props: { verbs }

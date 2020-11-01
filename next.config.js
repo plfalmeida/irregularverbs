f65cb81316608171fc5bdb4d5d326e069e9e3441
Pedro Almeida <pedro--luiz@outlook.com>
2020-11-01 18:55:04 -0300

    fix: remove asset prefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-    assetPrefix: isProd ? 'http://artemis.rec.br/nextjs/' : '',
     images: {
         domains: [
             'images.unsplash.com',
